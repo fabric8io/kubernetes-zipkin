307708ef6d2beccb9574b8729d4a2cf4e066dac8
Ioannis Canellos <iocanel@gmail.com>
2016-04-26 15:13:20 +0300

    Add a first draft of arquillian integration tests for starter-minimal and starter-mysql.

---
+++ starter-mysql/src/test/java/io/fabric8/zipkin/starter/mysql/ZipkinMysqlKubernetesTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2016 to origin authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fabric8.zipkin.starter.mysql;
+
+
+import io.fabric8.zipkin.starter.minimal.ZipkinMinimalKubernetesTest;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ZipkinMysqlKubernetesTest extends ZipkinMinimalKubernetesTest {
+
+}
